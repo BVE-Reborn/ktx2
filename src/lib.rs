1d32f5073352c1afff5429193beacfc2309b37e7
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-10-02 00:38:35 -0700

    Remove unneccessary static

--- src/lib.rs
+++ src/lib.rs
@@ -138,7 +138,7 @@ impl<Data: AsRef<[u8]>> Reader<Data> {
 }
 
 /// Identifier, expected in start of input texture data.
-static KTX2_IDENTIFIER: [u8; 12] = [
+const KTX2_IDENTIFIER: [u8; 12] = [
     0xAB, 0x4B, 0x54, 0x58, 0x20, 0x32, 0x30, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A,
 ];
 
